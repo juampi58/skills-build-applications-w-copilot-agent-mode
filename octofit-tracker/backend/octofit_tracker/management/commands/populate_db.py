fcb5e8a6bd46b244aa479926c7297691a7cde7a5
Juan Loza <juanpabloloza@gmail.com>
2025-04-11 06:50:56 +0000

    added all backend

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,56 @@
+from django.core.management.base import BaseCommand
+from tracker.models import User, Team, Activity, Leaderboard, Workout
+from bson import ObjectId
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create users
+        users = [
+            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
+            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
+            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
+        ]
+        User.objects.bulk_create(users)
+
+        # Create teams
+        team1 = Team(_id=ObjectId(), name='Blue Team')
+        team2 = Team(_id=ObjectId(), name='Gold Team')
+        team1.save()
+        team2.save()
+        team1.members.add(users[0], users[1])
+        team2.members.add(users[2], users[3], users[4])
+
+        # Create activities
+        activities = [
+            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=timedelta(hours=1)),
+            Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
+        ]
+        Activity.objects.bulk_create(activities)
+
+        # Create leaderboard entries
+        leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), user=users[0], score=100),
+            Leaderboard(_id=ObjectId(), user=users[4], score=80),
+        ]
+        Leaderboard.objects.bulk_create(leaderboard_entries)
+
+        # Create workouts
+        workouts = [
+            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a road cycling event'),
+            Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
+        ]
+        Workout.objects.bulk_create(workouts)
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
